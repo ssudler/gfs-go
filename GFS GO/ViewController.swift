f33d49d600184e944d88317e5db9a09747be7674
studente <studente@us-lab-19.local>
2019-03-05 10:22:40 -0500

    Initial Commit

---
+++ GFS GO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GFS GO
+//
+//  Created by studente on 3/5/19.
+//  Copyright © 2019 MadLads. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ GFS GOTests/GFS_GOTests.swift
@@ -0,0 +1,34 @@
+//
+//  GFS_GOTests.swift
+//  GFS GOTests
+//
+//  Created by studente on 3/5/19.
+//  Copyright © 2019 MadLads. All rights reserved.
+//
+
+import XCTest
+@testable import GFS_GO
+
+class GFS_GOTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
